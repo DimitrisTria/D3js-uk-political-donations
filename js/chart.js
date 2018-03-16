bddd3f1f3f85797c45408942d9664e27d771ab29
DimitrisTria <jimo4879898094@hotmail.com>
2018-03-16 16:36:06 +0200

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -151,7 +151,7 @@ var amount, offset, imagePath, infoBox;
 
 function mouseoverCircle(d) {
     // tooltip popup
-    imagePath = "https://raw.githubusercontent.com/ioniodi/D3js-uk-political-donations/master/photos/" + donor + ".ico";
+    imagePath = "https://raw.githubusercontent.com/ioniodi/D3js-uk-political-donations/master/photos/" + d.donor + ".ico";
     amount = d3.select(this).attr("amount");
     offset = $("svg").offset();
     d3.select(this).classed("active", true);
