bc81900b1940873edbff17f9b4130943a9264537
DimitrisTria <jimo4879898094@hotmail.com>
2018-03-23 10:34:46 +0200

    Update chart_interacting.js

--- js/chart_interacting.js
+++ js/chart_interacting.js
@@ -20,7 +20,7 @@ function mouseoverCircle(d) {
         .style("top", ((parseInt(d3.select(this).attr("cy") - (d.radius + 150)) + offset.top) - 13) + "px")
         .style("z-index", 2).html(infoBox).style("display", "block");
 
-    // responsiveVoice.speak(":" +d.donor +": with total value :" +comma(amount) +" pounds");
+    responsiveVoice.speak(":" +d.donor +": with total value :" +comma(amount) +" pounds");
     addImagesToHistoryBar(d, imagePath, amount);
 }
 
@@ -28,7 +28,7 @@ function mouseoutCircle() {
     /* no more tooltips */
     d3.select(this).classed("active", false);
     d3.select(".tooltip").style("display", "none"); /* */
-    // responsiveVoice.cancel();
+    responsiveVoice.cancel();
 }
 
 function clickCircle(d) {
@@ -61,11 +61,11 @@ function addImagesToHistoryBar(d, imagePath, amount) {
     imgNode.onmouseover = function () {
         donorsNameElement.innerHTML = "<p class='myDefaultClass' style='color:" + newDColor[d.color] + "; border:2px solid black; \n\
                                           background-color:#ffffcc; width:350px; text-allign:center;'>" + d.donor + "</p>";
-        // responsiveVoice.speak(":" + d.donor + ": with total value :" + comma(amount) + " pounds");
+        responsiveVoice.speak(":" + d.donor + ": with total value :" + comma(amount) + " pounds");
     };
     imgNode.onmouseout = function () {
         donorsNameElement.innerHTML = "";
-        // responsiveVoice.cancel();
+        responsiveVoice.cancel();
     };
     newImgElement.appendChild(imgNode);
 
